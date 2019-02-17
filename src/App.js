ba986f00884951ff9e2a976317e508af84b4ec5b
Randeep Singh <randeeprattan90@gmail.com>
2019-02-17 22:09:59 +0000

    Link each move to movieForm component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
@@ -14,6 +15,7 @@ class App extends Component {
         <NavBar />
         <main role="main" className="container">
           <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
