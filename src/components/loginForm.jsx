cba9e1582727d0e9dcbf90fbc926cb7a8e129085
Randeep Singh <randeeprattan90@gmail.com>
2019-02-18 20:28:17 +0000

    Added JOI library for validation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "./common/input";
 
 class LoginForm extends Component {
@@ -7,7 +8,18 @@ class LoginForm extends Component {
     errors: {}
   };
 
+  // set scehma, doesnt need to be part of the state as it is not supposed to change
+  schema = {
+    username: Joi.string().required(),
+    password: Joi.string().required()
+  };
+
   validate = () => {
+    const result = Joi.validate(this.state.account, this.schema, {
+      abortEarly: false
+    });
+    console.log(result);
+
     const errors = {};
 
     const { account } = this.state;
