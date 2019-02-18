8aa83e5ad1469ae80132336f1d36ecbdb4dd254f
Randeep Singh <randeeprattan90@gmail.com>
2019-02-18 20:13:16 +0000

    Validation on change

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -31,10 +31,28 @@ class LoginForm extends Component {
     // call the server
   };
 
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required";
+      //... more rules here
+    }
+
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required";
+      //... more rules here
+    }
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+
+    this.setState({ account, errors });
   };
 
   render() {
