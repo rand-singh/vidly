a481741525fd721872271210b0c1c82077acc9df
Randeep Singh <randeeprattan90@gmail.com>
2019-02-18 19:59:51 +0000

    Displaying validation errors on form submit

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -25,8 +25,7 @@ class LoginForm extends Component {
     e.preventDefault();
 
     const errors = this.validate();
-    console.log(errors);
-    this.setState({ errors });
+    this.setState({ errors: errors || {} });
     if (errors) return;
 
     // call the server
@@ -39,7 +38,7 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
 
     return (
       <div>
@@ -50,6 +49,7 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
 
           <Input
@@ -57,6 +57,7 @@ class LoginForm extends Component {
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
 
           <button className="btn btn-primary">Login</button>
