69dc1c9c93c1a8434e6e35bacbf45c5ea3844538
Randeep Singh <Randeep.SinghRattan@howdens.com>
2019-03-04 22:39:09 +0000

    Redirecting after login

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 import Joi from "joi-browser";
 import Form from "./common/form";
 import auth from "../services/authService";
@@ -25,8 +26,9 @@ class LoginForm extends Form {
 
       // store the Json Web Token on client side
       await auth.login(data.username, data.password);
+      const { state } = this.props.location;
 
-      window.location = "/";
+      window.location = state ? state.from.pathname : "/";
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...this.state.errors };
@@ -37,6 +39,7 @@ class LoginForm extends Form {
   };
 
   render() {
+    if (auth.getCurrentUser()) return <Redirect to="/" />;
     return (
       <div>
         <h1>Login</h1>
